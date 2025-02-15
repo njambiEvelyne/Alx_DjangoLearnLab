11402a9b16fabfa177f1ad4ed7a40ed652f3b145
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-15 13:35:17 +0300

    Added project models

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bookshelf',
 ]
 
 ROOT_URLCONF = 'LibraryProject.urls'
