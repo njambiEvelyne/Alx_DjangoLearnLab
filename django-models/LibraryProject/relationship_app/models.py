3f5357ba3d68ed24588574f18ad26d8f4143bb32
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-21 11:20:09 +0300

    Adde author model and applied migrations

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -30,3 +30,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
