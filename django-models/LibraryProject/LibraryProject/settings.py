8d82c37a1a97a48c4f2dd40876488a31b255ee0d
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-21 08:39:48 +0300

    Added function-based and class-based views with URL routing and templates

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'bookshelf.middleware.CustomMiddleware', 
+    
 ]
 
 ROOT_URLCONF = 'LibraryProject.urls'
