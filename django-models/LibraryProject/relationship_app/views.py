02c2e5e242f38ed7a71e46eeb92825ff5f2ad4c8
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-21 09:13:34 +0300

    Modified db.sqlite

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library, Book  # ✅ Import Library and Book models
 
 # Function-Based View to list all books
