4d2088b68a7ab447298df1c67dda49658bafe602
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-05 17:10:13 +0300

    Corrected a model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, BookList
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # path('books/', BookList.as_view(), name='book-list'),  # API endpoint for listing books
+    path('books/', BookList.as_view(), name='book-list'),  # API endpoint for listing books
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,6 +6,10 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+class BookList(ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all books
+    serializer_class = BookSerializer
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
