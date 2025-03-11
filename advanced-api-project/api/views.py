be70e8a91d19c82ba6256f7b4a06108b2b00d07e
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-11 15:43:06 +0300

    Added a filter library

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Book
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
