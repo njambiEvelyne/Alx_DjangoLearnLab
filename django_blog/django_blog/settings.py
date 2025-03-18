2a37cb9d3bb88b99f042044d7b19a7f4620e527e
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-18 07:55:29 +0300

    Added database configurations

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -83,8 +83,15 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        'NAME': 'alx',
+        'USER': 'root',
+        'PASSWORD': '20Eve,lyne#76',
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
