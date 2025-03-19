4743848d491df1764c77e591be513bbe822782a7
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-19 18:48:06 +0300

    Added new attributes for the created tables

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Post(models. Model):
   title = models.CharField(max_length=200)
   content = models.TextField()
   published_date = models.DateTimeField(auto_now_add=True)
   author = models.ForeignKey(User, on_delete= models.CASCADE)
+  created_at = models.DateTimeField(default=timezone.now)
+  updated_at = models.DateTimeField(auto_now=True)
   
