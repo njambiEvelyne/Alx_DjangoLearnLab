4b33111bb38c57b1521f759388b14d35141be1e5
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-21 17:45:41 +0300

    Added a new file

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -49,6 +49,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    tags = TaggableManager()  # Enables tagging
+
 
     def __str__(self):
         return self.title
