0ad79a43d1808e12dd5cb79e20be2a6c692ec147
njambiEvelyne <evelynenganga76@gmail.com>
2025-03-21 17:41:24 +0300

    Added new templates

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post, Tag
-from .models import Comment
-from taggit.forms import TagWidget
+from taggit.forms import TagWidget  # Import TagWidget
+from .models import Post, Comment
 
 
-
+# User Registration Form
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -14,47 +13,29 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+
+# Profile Edit Form
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username", "email"]
 
+
+# Post Form with Tags
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']  
+        widgets = {
+            'tags': TagWidget(attrs={'class': 'form-control'}),  # Use TagWidget for tag input
+        }
 
+
+# Comment Form
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'class': 'form-control', 'placeholder': 'Write a comment...'})
-        }
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-
-class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Enter tags separated by commas.")
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
-
-    def clean_tags(self):
-        tag_names = self.cleaned_data.get('tags', '').split(',')
-        tags = [Tag.objects.get_or_create(name=tag.strip())[0] for tag in tag_names if tag.strip()]
-        return tags
-    
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']  
-
-        widgets = {
-            'tags': TagWidget(attrs={'class': 'form-control'}), 
         }
